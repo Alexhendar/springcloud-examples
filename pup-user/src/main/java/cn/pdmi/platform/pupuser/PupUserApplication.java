cec9cd9eeb1612f88b5a523798f234f717bb91bc
zhangjunyong <zhangjunyong@pdmi.cn>
2018-06-22 10:41:37 +0800

    读取jar包中的id_rsa文件需要用Stream方式读取 报错:[cannot be resolved to absolute file path because it does not reside in the file system]

--- pup-user/src/main/java/cn/pdmi/platform/pupuser/PupUserApplication.java
+++ pup-user/src/main/java/cn/pdmi/platform/pupuser/PupUserApplication.java
@@ -1,9 +1,8 @@
 package cn.pdmi.platform.pupuser;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import org.apache.commons.lang.StringUtils;
@@ -11,7 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.jwt.crypto.sign.RsaSigner;
-import org.springframework.util.ResourceUtils;
 
 @SpringBootApplication
 public class PupUserApplication {
@@ -19,24 +17,21 @@ public class PupUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PupUserApplication.class, args);
 	}
-	
+
 	@Bean
 	public RsaSigner jwtSigner() {
 		String privateKey = StringUtils.EMPTY;
 		try {
-			File keyFile = ResourceUtils.getFile("classpath:id_rsa");
-			if(keyFile.exists()) {
-				InputStreamReader reader = new InputStreamReader(new FileInputStream(keyFile));
-				StringBuffer buffer = new StringBuffer();
-				int tmp;
-				while((tmp = reader.read())!=-1) {
-					buffer.append((char)tmp);
-				}
-				privateKey = buffer.toString();
-				reader.close();
-			}else {
-				System.out.println("private key file not exist!");
+			InputStream ips = PupUserApplication.class.getResourceAsStream("/id_rsa");
+			// File keyFile = ResourceUtils.getFile("classpath:id_rsa");
+			InputStreamReader reader = new InputStreamReader(ips);
+			StringBuffer buffer = new StringBuffer();
+			int tmp;
+			while ((tmp = reader.read()) != -1) {
+				buffer.append((char) tmp);
 			}
+			privateKey = buffer.toString();
+			reader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
--- pup-user/src/test/java/cn/pdmi/platform/pupuser/JsonParserTest.java
+++ pup-user/src/test/java/cn/pdmi/platform/pupuser/JsonParserTest.java
@@ -1,5 +1,11 @@
 package cn.pdmi.platform.pupuser;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import net.sf.json.JSONObject;
@@ -20,4 +26,25 @@ public class JsonParserTest {
 		System.out.println(json.toString());
 	}
 
+	@Test
+	public void testPrivateKey() {
+		String privateKey = StringUtils.EMPTY;
+		try {
+			InputStream ips = JsonParserTest.class.getResourceAsStream("/id_rsa");
+			InputStreamReader reader = new InputStreamReader(ips);
+			StringBuffer buffer = new StringBuffer();
+			int tmp;
+			while ((tmp = reader.read()) != -1) {
+				buffer.append((char) tmp);
+			}
+			privateKey = buffer.toString();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(privateKey);
+	}
 }
